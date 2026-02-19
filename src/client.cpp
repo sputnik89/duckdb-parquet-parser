3fc927d0e0638b1b0f4371674f89f1e9b85b50a4
Kaiwen Zheng <helloworld.zheng@mail.utoronto.ca>
2026-02-19 16:40:15 -0500

    WIP: reading from DPK and reassemling output parquet file

---
+++ src/client.cpp
@@ -0,0 +1,93 @@
+#include "parquet_reader.hpp"
+#include <iostream>
+#include <utility>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define SERVER_PORT (8080)
+
+typedef std::pair<char*, uint8_t> string_t;
+
+class StringPageReader {
+public:
+    StringPageReader(char* bytes, size_t nbytes) :
+            bytes_(bytes), nbytes_(nbytes), cursor_(0) {}
+
+    string_t read(size_t offset) {
+        if (offset >= nbytes_ || offset == 0) {
+            return std::make_pair(nullptr, 0);
+        } else {
+            uint8_t len = *reinterpret_cast<uint8_t*>(bytes + offset - 1);
+            char* string = bytes + offset;
+            return std::make_pair(string, len);
+        }
+    }
+private:
+    char* bytes_;
+    size_t nbytes_;
+    size_t cursor_;
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "usage: ./client <filepath> <serverip>\n");
+        exit(1);
+    }
+    
+    int sockfd;
+    struct sockaddr_in server_addr;
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    // configure server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    char* serverip = argv[2];
+    if (inet_pton(AF_INET, serverip, &server_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        close(sockfd);
+        exit(1);
+    }
+
+    // connect to server
+    if (connect(sockfd, (struct sockaddr_in*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect");
+        close(sockfd);
+        exit(1);
+    }
+
+    // send pages one by one
+    ParquetReader reader;
+    assert(!reader.open(argv[1]));
+    PageIterator itr = reader.page_iterator();
+    while (itr.has_next()) {
+        RawPage page = itr.next();
+        std::vector<uint8_t> page_data = page.data;
+        char* msg = reinterpret_cast<char*>(page_data.data());
+        size_t nbytes_total = page_data.size();
+        size_t nbytes_sent = 0;
+        ssize_t sent;
+        // send all of the current page to server
+        while (nbytes_sent < nbytes_total) {
+            if ((sent = send(sockfd, msg+nbytes_sent, nbytes_total-nbytes_sent, 0)) < 0) {
+                exit(1);
+            }
+            nbytes_sent += sent;
+        }
+        // wait for DPK regex to finish preprocessing and read the outputs
+        
+        // TODO: read all of the current response from server
+        StringPageReader page_reader(page_data.data(), nbytes_total);
+        size_t offsets[] = {};  // TODO:
+        size_t n_matches = 0;  // TODO:
+        for (size_t i = 0; i < n_matches; i++) {
+            string_t match = page_reader.read(offsets[i]);
+        }
+    }
+
+}
